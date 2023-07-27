b54dd63260479df5017b8760cfd6f50fd9579ea6
Suhaib Abdirahman <139591958+Suhaib-Abdirahman@users.noreply.github.com>
2023-07-27 22:46:39 +0300

    Done

--- challenge.js
+++ challenge.js
@@ -1,15 +1,13 @@
-// Good Luck! You got this 💪🏾
-// Write your code here.
-function calculator(num1, num2, operator) {
-  if (operator === "+") {
-    console.log(num1 + num2);
-  } else if (operator === "-") {
-    console.log(num1 - num2);
-  } else if (operator === "*") {
-    console.log(num1 * num2);
-  } else if (operator === "/") {
-    console.log(num1 / num2);
+function playGame(player1, player2) {
+  if (player1 === player2) {
+    return "tie";
+  } else if (
+    (player1 === "rock" && player2 === "scissors") ||
+    (player1 === "paper" && player2 === "rock") ||
+    (player1 === "scissors" && player2 === "paper")
+  ) {
+    return "player1 wins";
   } else {
-    console.log("Invalid operator.");
+    return "player2 wins";
   }
 }
